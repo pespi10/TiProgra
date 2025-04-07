ec3dacf81f78cc56f3b804220b292cdc5ac174aa
felubarreiro <fbarreiro@udesa.edu.ar>
2025-04-07 18:38:41 -0300

    modulo de datos

--- TI/db/usuario.js
+++ TI/db/usuario.js
@@ -1,15 +1,213 @@
-const infou= [
+const infou= {
 
-usuario=[
+usuario: {
+    nombre:"",
+    email:"",
+    contraseña:"",
+    fecha_nacimiento:"",
+    documento:"",
+    foto_perfil:""
 
-],
+}
 
-productos= [
+,
 
-],
+productos: [
+    producto1= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
 
-comentarios = [
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto2= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto3= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto4= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto5= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto6= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto7= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto8= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto9= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    producto10= {
+        imagen_producto:"",
+        comentarios:[
+            comentario1={
+
+            },
+            comentario2={
+
+            },
+            comentario3={
+            usuario:"",
+            texto_comentario:"",
+            imagen_perfil:""
+            }],
+        nombre: "",
+        descripcion: "",
+        imagen:"",
+
+    },
+    
+
 
 ]
 
-]
+}
+
+module.exports = infou
