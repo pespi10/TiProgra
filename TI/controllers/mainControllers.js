2e4a4d6601b5fce19434279bb2a045813e7d0ec0
Lucas <lfierro@udesa.edu.ar>
2025-04-13 16:21:05 -0300

    productos.js

--- TI/controllers/mainControllers.js
+++ TI/controllers/mainControllers.js
@@ -1,29 +1,29 @@
-
-const infou = require ("../db/usuario")
+const usuario = require ("../db/usuario")
+const productos = require ("../db/productos")
 
 const mainController={
 
   index:function(req, res) {
-    res.render('index',{productos:infou.productos, usuario:infou.usuario});
+    res.render('index',{productos:productos, usuario:usuario});
   },
   login: function(req, res) {
-    res.render('login',{usuario:infou.usuario, productos:infou.productos});
+    res.render('login',{usuario:usuario, productos:productos});
   },
   register: function(req, res) {
-    res.render('register',{usuario:infou.usuario, productos:infou.productos});
+    res.render('register',{usuario:usuario, productos:productos});
   },
   product: function(req, res) {
-    res.render('product',{productos:infou.productos, usuario:infou.usuario});
+    res.render('product',{productos:productos, usuario:usuario});
   },
   profile: function(req, res) {
-    res.render('profile',{usuario:infou.usuario,productos:infou.productos}
+    res.render('profile',{usuario:usuario,productos:productos}
     );
   },
   productAdd: function(req, res) {
-    res.render('product-add',{productos:infou.productos, usuario:infou.usuario});
+    res.render('product-add',{productos:productos, usuario:usuario});
   },
   searchResults: function(req, res) {
-    res.render('search-results',{productos:infou.productos, usuario:infou.usuario});
+    res.render('search-results',{productos:productos, usuario:usuario});
   },
 }
 
---
+++ TI/db/productos.js
@@ -0,0 +1,224 @@
+const productos = [
+    {
+        imagen: "/images/products/teclado.png",
+        nombre: "Teclado inalambrico",
+        descripcion: "Swithces mecánicos, retroiluminado RGB, conexión USB-C",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/mouse.png",
+        nombre: "Mouse",
+        descripcion: "sensor optico de alta precisión, luces LED RGB, 16000 DPI",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/monitor.png",
+        nombre: "Monitor curvo 4k",
+        descripcion: "Monitor curvo 4k de 32 pulgadas, 144Hz, FreeSync",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/notebook.png",
+        nombre: "Notebook",
+        descripcion: "Notebook HP 15, i5, 8GB RAM, 512GB SSD",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/",
+        nombre: "",
+        descripcion: "",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/",
+        nombre: "",
+        descripcion: "",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/",
+        nombre: "",
+        descripcion: "",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/",
+        nombre: "",
+        descripcion: "",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/",
+        nombre: "",
+        descripcion: "",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    },
+    {
+        imagen: "/images/products/",
+        nombre: "",
+        descripcion: "",
+        comentarios: [
+            {
+                usuario: "Ana García",
+                userComment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "Carlos López",
+                userComment: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                imagenPerfil: "/images/users/default-image.png"
+            },
+            {
+                usuario: "María Rodríguez",
+                userComment: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                imagenPerfil: "/images/users/default-image.png"
+            }
+        ]
+    }
+]
+
+Module.exports = productos;
